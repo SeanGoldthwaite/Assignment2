1bffc952f516ae1a96c99d3e8bf69b1d63319fcf
Sean Goldthwaite <seangoldthwaite@comcast.net>
2020-01-23 23:03:42 -0500

    LinkedList

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -8,30 +8,41 @@ class LinkedList<E> implements LinkedListI<E> {
     }
 
     public void clear() {
-
+        elements = new EmptyL<>();
     }
 
     public int size() {
-        return 0;
+        return elements.length();
     }
 
     public void addFirst(E elem) {
-
+        elements = new NodeL<>(elem, elements);
     }
 
     public void addLast(E elem) {
-
+        //elements = new NodeL<>(elements.getFirst(), elements.getRest().addLast(elem));
     }
 
     public E getFirst() throws NoSuchElementE {
-        return null;
+        if (elements.isEmpty())
+            throw new NoSuchElementE();
+        else
+            return elements.getFirst();
     }
 
     public E getLast() throws NoSuchElementE {
-        return null;
+        if (elements.isEmpty())
+            throw new NoSuchElementE();
+        else
+            return elements.getLast();
     }
 
     public E removeFirst() throws NoSuchElementE {
-        return null;
+        E temp = elements.getFirst();
+        if (elements.isEmpty())
+            throw new NoSuchElementE();
+        else
+            elements = new NodeL<>(elements.getRest().getFirst(), elements.getRest().getRest());
+            return temp;
     }
 }
